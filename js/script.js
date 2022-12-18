1f61fc90c10e8b1e9920a877b3717de908bcfc3f
FreightDH <maxim.zolotkov@yandex.ru>
2022-12-19 02:06:47 +0300

    feat: add responsive

---
+++ js/script.js
@@ -0,0 +1,32 @@
+function burger() {
+	$(document).ready(function() {
+		$(document).on('click', '.header-menu__icon', function(event) {
+			if ($('.header-menu__icon').hasClass('active')) {
+				$('.header-menu__menu').removeClass('active');
+				$('.header-menu__icon').removeClass('active');
+				$('html, body').css('overflow', '');
+			}
+			else {
+				$('.header-menu__menu').addClass('active');
+				$('.header-menu__icon').addClass('active');
+				$('html, body').css('overflow', 'hidden');
+			}
+			return false;
+		}); 
+	});
+}
+
+function linkClick() {
+	$(document).ready(function () {
+		$(document).on('click', '.header-menu-list__link', function(event) {
+			if ($('.header-menu__menu').hasClass('active')) {
+				$('.header-menu__menu').removeClass('active');
+				$('.header-menu__icon').removeClass('active');
+				$('html, body').css('overflow', '');
+			}
+		});
+	});
+}
+
+burger();
+linkClick();
